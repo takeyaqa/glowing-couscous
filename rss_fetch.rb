b9b9982e69040a77266f14792400411270aa0902
Takeshi Kishi <takeyaqa@icloud.com>
2026-09-09 12:33:31 +0900

    change

--- rss_fetch.rb
+++ rss_fetch.rb
@@ -301,7 +301,6 @@ if __FILE__ == $0
   result = rss_fetch.collect_feeds(**runtime_options)
   article_count = result.feeds.sum { |feed| feed[:items].length }
   warn "Complete: articles=#{article_count}"
-  warn result.errors if result.errors?
   puts JSON.pretty_generate(result.to_h)
   exit result.errors? ? 1 : 0
 end
